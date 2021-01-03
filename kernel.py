6e213160fa9a5ae24c80117a5ed6d25d7647ef01
Ruslan Baikulov <ruslan1123@gmail.com>
2021-01-03 03:32:06 +0300

    fix: Fix dataset name typo

--- kernel.py
+++ kernel.py
@@ -8,6 +8,6 @@ def run(command):
 
 run("pip install /kaggle/input/ranzcr-clip-dataset/pytorch_argus-0.2.0-py3-none-any.whl")
 run("pip install /kaggle/input/ranzcr-clip-dataset/timm-0.3.2-py3-none-any.whl")
-run("cp -r /kaggle/input/anzcr-clip-dataset/anzcr-clip/* .")
+run("cp -r /kaggle/input/ranzcr-clip-dataset/ranzcr-clip/* .")
 
 run("python predict_kernel.py")
