a828b3099d3e73a00f1ba250e951b80b35bf2b4d
Ruslan Baikulov <ruslan1123@gmail.com>
2021-01-07 19:55:40 +0300

    fix: Fix checkout in download script

--- data/ranzcr-deps/download.sh
+++ data/ranzcr-deps/download.sh
@@ -6,8 +6,7 @@ pip3 download --no-deps \
     pytorch-argus==0.2.0
 
 rm -rf ./ranzcr-clip
-git clone git@github.com:lRomul/ranzcr-clip.git
-[[ !  -z  "$2"  ]] && cd ranzcr-clip && git checkout "$1" && cd ..
+git clone git@github.com:lRomul/ranzcr-clip.git && cd ranzcr-clip && git checkout "$1" && cd ..
 
 cp -r "../experiments/$1" "./ranzcr-clip/data/experiments/$1"
 sed "s/experiment_name/$1/g" ./ranzcr-clip/kernel.py > ./kernel.py
