9f378b780607fdfae94e804760110854e267b01e
Ruslan Baikulov <ruslan1123@gmail.com>
2021-01-31 22:54:23 +0300

    fix: Copy cls models to data/experiments

--- data/ranzcr-deps/download.sh
+++ data/ranzcr-deps/download.sh
@@ -6,5 +6,5 @@ git clone git@github.com:lRomul/ranzcr-clip.git && cd ranzcr-clip && git checkou
 rm -rf ./requirements
 pip3 download --no-deps -d requirements -r ranzcr-clip/requirements.txt
 
-cp -r "../experiments/$1" "./ranzcr-clip/data/segm/experiments/$1"
+cp -r "../experiments/$1" "./ranzcr-clip/data/experiments/$1"
 sed "s/experiment_name/$1/g" ./ranzcr-clip/kernel.py > ./kernel.py
