68643a711413334de6546b89c1610572e21fea99
Ruslan Baikulov <ruslan1123@gmail.com>
2021-02-23 23:08:33 +0300

    feat: Build wheels for archive requirements

--- data/ranzcr-deps/download.sh
+++ data/ranzcr-deps/download.sh
@@ -12,6 +12,13 @@ git clone git@github.com:lRomul/ranzcr-clip.git && cd ranzcr-clip && git checkou
 rm -rf ./requirements
 pip3 download --no-deps -d requirements -r ranzcr-clip/requirements.txt
 
+for filename in requirements/*.{zip,tar.gz}
+do
+  [ -e "$filename" ] || continue
+	pip3 wheel --no-deps -w requirements "$filename"
+	rm "$filename"
+done
+
 sed "s/experiment_name/$EXPS/g" ./ranzcr-clip/kernel.py > ./kernel.py
 for EXP in "${EXPS_ARR[@]}"
 do
--- kernel.py
+++ kernel.py
@@ -8,9 +8,7 @@ def run(command):
               f"export KERNEL_MODE=predict && " + command)
 
 
-run("cp -r /kaggle/input/ranzcr-clip-dataset/requirements .")
-run("cd requirements && find * -maxdepth 0 -type d -exec tar czvf {}.tar.gz -C {} {} \; && rm -R -- */")
-run("pip install --force-reinstall --no-deps requirements/*")
+run("pip install --force-reinstall --no-deps /kaggle/input/ranzcr-clip-dataset/requirements/*")
 run("cp -r /kaggle/input/ranzcr-clip-dataset/ranzcr-clip/* .")
 
 run(f"python predict.py --experiment {EXPERIMENT}")
