56fd0d49aafa6068ac5ebac970a725568f95cb70
Ruslan Baikulov <ruslan1123@gmail.com>
2021-02-27 10:31:43 +0300

    Train experiment b7_010

--- src/transforms.py
+++ src/transforms.py
@@ -38,9 +38,7 @@ def get_transforms(train: bool, size: int, n_channels: int,
                                  scale_limit=0.2, rotate_limit=20,
                                  interpolation=interpolation,
                                  border_mode=border_mode),
-            alb.CoarseDropout(p=0.2),
-            alb.Cutout(p=0.2, max_h_size=16, max_w_size=16,
-                       fill_value=0., num_holes=16),
+            alb.CoarseDropout(p=0.8, max_width=64, max_height=64, max_holes=16),
             alb.Normalize(mean=mean[:n_channels], std=[std[:n_channels]]),
             alb.pytorch.ToTensorV2()
         ])
