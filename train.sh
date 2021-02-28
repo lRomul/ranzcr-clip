f0d31bbc94ed070f91631150e9dd6bb52117dcda
Ruslan Baikulov <ruslan1123@gmail.com>
2021-02-28 11:29:24 +0300

    Train experiment b7_012

--- train.sh
+++ train.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 
 EXP=${1}
 N_DEVICE=${2:-$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)}
 DEVICE=${3:-all}
-FOLDS=${4:-0,1,2,3,4}
+FOLDS=${4:-all}
 
 echo "EXP=$EXP, N_DEVICE=$N_DEVICE, DEVICE=$DEVICE, FOLDS=$FOLDS"
 
-git fetch --all
-
-for fold in ${FOLDS//[,]/ }
-do
-  git reset --hard "$EXP" &&\
-  make GPUS="'\"device=$DEVICE\"'" \
-    COMMAND="python -m torch.distributed.launch --nproc_per_node=$N_DEVICE train_cls.py --experiment $EXP --folds $fold" \
-    NAME="ranzcr-clip-$EXP-$fold"
-done
+cd "$(dirname "${BASH_SOURCE[0]}")" &&\
+git fetch --all &&\
+git reset --hard "$EXP" &&\
+make GPUS="'\"device=$DEVICE\"'" \
+  COMMAND="python -m torch.distributed.launch --nproc_per_node=$N_DEVICE train_cls.py --experiment $EXP --folds $FOLDS" \
+  NAME="ranzcr-clip-$EXP-${FOLDS//[,]/}"
